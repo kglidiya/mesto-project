82f77c112c453196ca44819a0c6a0b7e447c5efc
kglidiya <betysmith@yandex.ru>
2022-03-29 14:38:55 +0300

    third commit

--- script.js
+++ script.js
@@ -23,22 +23,26 @@ function openPopup(popup) {
   popup.classList.add('popup_opened');
 };
 
-buttonEdit.addEventListener('click', () => openPopup(profilePopup));
+buttonEdit.addEventListener('click', function () {
+  nameInput.value = userName.textContent;
+  jobInput.value = userProffesion.textContent;
+  openPopup(profilePopup)
+});
+
 buttonAdd.addEventListener('click', () => openPopup(cardPopup));
 
 //Попап с фото открывается отдельно, т.к. там идет привязка к ссылке на фото 
 
 
 //Закрытие всех попапов
+
 function closePopup(popup) {
   popup.classList.remove('popup_opened');
 };
 
-buttonsClose.forEach(function (btn) {
-  btn.addEventListener('click', () => closePopup(profilePopup));
-  btn.addEventListener('click', () => closePopup(cardPopup));
-  btn.addEventListener('click', () => closePopup(imagePopup));
-});
+buttonsClose.forEach(btn => btn.addEventListener('click', () => closePopup(profilePopup)));
+buttonsClose.forEach(btn => btn.addEventListener('click', () => closePopup(cardPopup)));
+buttonsClose.forEach(btn => btn.addEventListener('click', () => closePopup(imagePopup)));
 
 
 //Заполение формы профиля
@@ -50,54 +54,6 @@ function formSubmitHandler(evt) {
   closePopup(profilePopup);
 };
 formElementProfile.addEventListener('submit', formSubmitHandler);
-
-
-//Создание  новой карточки из формы клонированием template
-
-function addNewPhoto() {
-  const placesElement = cardsTemplate.cloneNode(true);
-  placesElement.querySelector('.places__title').textContent = placeInput.value;
-  placesElement.querySelector('.places__image').src = linkInput.value;
-
-  //Добавление кнопок лайка и удаления карточки
-  placesElement.querySelector('.places__likebtn').addEventListener('click', function (btn) {
-    btn.target.classList.toggle('places__likebtn_active');
-  });
-
-  placesElement.querySelector('.places__deletebtn').addEventListener('click', function (btn) {
-    const placeContainer = btn.target.closest('.places__container');
-    placeContainer.remove();
-  });
-
-  cardsContainer.prepend(placesElement);
-
-  //Открытие попапа с новой карточкой
-  const cardContainer = document.querySelector('.places__container');
-  const image = document.querySelector('.places__image');
-
-  image.addEventListener('click', function open() {
-    imagePopup.classList.add('popup_opened');
-    document.querySelector('.image-popup__photolink').src = this.src;
-  });
-
-  cardContainer.addEventListener('click', function showImage() {
-    document.querySelector('.image-popup__caption').textContent = this.textContent;
-  });
-
-};
-
-
-//Добавление карточки из формы
-
-function renderCard(evt) {
-  evt.preventDefault();
-  addNewPhoto();
-  closePopup(cardPopup);
-  placeInput.value = '';
-  linkInput.value = '';
-};
-formElementPlace.addEventListener('submit', renderCard);
-
 
 
 //Клонирование template
@@ -129,45 +85,53 @@ const initialCards = [
   }
 ];
 
+//Создание карточки
 
-function uploadInitialCards() {
+function creatCard(caption, image) {
+  const placesElement = cardsTemplate.cloneNode(true);
+  placesElement.querySelector('.places__title').textContent = caption;
+  placesElement.querySelector('.places__image').src = image;
 
-  initialCards.forEach(function (element) {
-    const placesElement = cardsTemplate.cloneNode(true);
-    placesElement.querySelector('.places__title').textContent = element.name;
-    placesElement.querySelector('.places__image').src = element.link;
-
-    //Добавление кнопок лайков и удаления
-    placesElement.querySelector('.places__likebtn').addEventListener('click', function (btn) {
-      btn.target.classList.toggle('places__likebtn_active');
-    });
-
-    placesElement.querySelector('.places__deletebtn').addEventListener('click', function (btn) {
-      const placeContainer = btn.target.closest('.places__container');
-      placeContainer.remove();
-    })
-
-    const initialCardsImage = document.querySelectorAll('.places__image');
-    const initialCardsImageContainer = document.querySelectorAll('.places__container');
-
-    //Извините, я не соовсем поняла комментарий "Все слушатели устанавливаются в момент создания карточки"
-    //Ведь вся эта фукция - загрузка массива с фото. 
-    initialCardsImage.forEach(function open(item) {
-      item.addEventListener('click', function openPopup() {
-        imagePopup.classList.add('popup_opened');
-        document.querySelector('.image-popup__photolink').src = this.src;
-
-        initialCardsImageContainer.forEach(function showCaption(elem) {
-          elem.addEventListener('click', function () {
-            document.querySelector('.image-popup__caption').textContent = this.textContent;
-          });
-        });
-      });
-    });
-
-    cardsContainer.append(placesElement)
+  //Добавление кнопок лайка и удаления карточки
+  placesElement.querySelector('.places__likebtn').addEventListener('click', function (btn) {
+    btn.target.classList.toggle('places__likebtn_active');
   });
 
+  placesElement.querySelector('.places__deletebtn').addEventListener('click', function (btn) {
+    const placeContainer = btn.target.closest('.places__container');
+    placeContainer.remove();
+  });
+
+  //Открытие попапа с карточкой
+  const cardOpen = placesElement.querySelector('.places__image');
+
+  cardOpen.addEventListener('click', function openCard() {
+    openPopup(imagePopup);
+    document.querySelector('.image-popup__photolink').src = this.src;
+    document.querySelector('.image-popup__photolink').alt = caption;
+    document.querySelector('.image-popup__caption').textContent = caption;
+
+  });
+
+  return placesElement;
 };
 
-uploadInitialCards();
+
+//Добавление карточки из массива
+
+initialCards.forEach(function (item) {
+  const card = creatCard(item.name, item.link);
+  cardsContainer.append(card);
+});
+
+//Добавление карточки из формы
+
+function renderCard(evt) {
+  evt.preventDefault();
+  cardsContainer.prepend(creatCard(placeInput.value, linkInput.value));
+  closePopup(cardPopup);
+  placeInput.value = '';
+  linkInput.value = '';
+};
+
+formElementPlace.addEventListener('submit', renderCard);
